c35a8d20424c1222f85ecb79357f84e77dc0ebba
MR.CSMADMAX <MR.CSMADMAX@LAPTOP-NQD1QHJR>
2024-11-18 15:36:33 +0530

    Add project files.

---
+++ RepositoryDesignPattern.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RepositoryDesignPattern.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
